b4718479075a238b32494c4f171d4ba82a22b697
Madhan <mmadhanjm@gmail.com>
2026-01-03 16:52:33 +0530

    refactor :[Madhan] refactored the code in the form of function

--- src/log_genrator.py
+++ src/log_genrator.py
@@ -2,31 +2,36 @@ from faker import Faker
 import random
 fake=Faker()
 
-levels=["DEBUG","INFO","WARNING","ERROR","CRITICAL"]
-description=[
-    "User logged in",
-    "File not found",
-    "Connection established",
-    "Data saved successfully",
-    "Error processing request",]
-status={
-    "DEBUG":"OK",
-    "INFO":"OK",
-    "WARNING":"FAIL",
-    "ERROR":"FAIL",
-    "CRITICAL":"CRASH"
-}
+def genarate_log(filename="log.txt",n=30):
 
-with open("log.txt","w") as f:
-    for _ in range(10):
-        level=random.choice(levels)
-        log=(
-            f"{fake.date_time()} | "
-            f"{level} | "
-            f"{random.choice(description)} | "
-            f"{fake.bothify(text="???###")} | "
-            f"{fake.ipv4()} | "
-            f"{status[level]} | "
-            f"{fake.http_status_code()}"
-        )
-        f.write(log+"\n")
+    levels=["DEBUG","INFO","WARNING","ERROR","CRITICAL"]
+    description=[
+        "User logged in",
+        "File not found",
+        "Connection established",
+        "Data saved successfully",
+        "Error processing request",]
+    status={
+        "DEBUG":"OK",
+        "INFO":"OK",
+        "WARNING":"FAIL",
+        "ERROR":"FAIL",
+        "CRITICAL":"CRASH"
+    }
+    
+    status_code=[200,201,400,401,500,503]
+
+    with open("log.txt","w") as f:
+        for _ in range(n):
+            level=random.choice(levels)
+            log=(
+                f"{fake.date_time()} | "
+                f"{level} | "
+                f"{random.choice(description)} | "
+                f"{fake.ipv4()} | "
+                f"{status[level]} | "
+                f"{random.choice(status_code)}"
+            )
+            f.write(log+"\n")
+            
+genarate_log()
