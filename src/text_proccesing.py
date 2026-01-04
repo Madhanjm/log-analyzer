8595073ef4ed959e81e2715c4eacf3967801375a
Madhan <mmadhanjm@gmail.com>
2026-01-04 14:07:18 +0530

    add :[Madhan] added data structures for unique ip and freq for httpcode

--- src/text_proccesing.py
+++ src/text_proccesing.py
@@ -1,6 +1,7 @@
 import re
 def text_analysis(input="log.txt",output="summary_log.txt"):
-    ip=[]
+    unique_ip=set()
+    code_freq={}
     warning_log=0
     error_log=0
     total_log=0
@@ -14,25 +15,38 @@ def text_analysis(input="log.txt",output="summary_log.txt"):
             date_time=split_log[0]
             level=split_log[1]
             description=split_log[2]
+            
             ip_address=split_log[3]
+            
+            
             status=split_log[4]
             http_code=split_log[5]
             
-            ip.append(ip_address)
+            unique_ip.add(ip_address)
             
             if level=="ERROR" or level=="CRITICAL":
                 error_log+=1
             elif level=="WARNING":
                 warning_log+=1
                 
+            if re.match(r"[245]\d{2}",http_code):
+                if http_code in code_freq:
+                    code_freq[http_code]+=1
+                else:
+                    code_freq[http_code]=1
+                        
     with open(output,"w") as f:
         f.write("---------------Log Summary------------------------\n")
         f.write(f"Total Logs : {total_log}\n")
         f.write(f"Error Logs : {error_log}\n")
         f.write(f"Warning Logs : {warning_log}\n")
-        f.write("Collected Ips : \n")
-        for i in ip:
+        f.write("Unique Ips : \n")
+        for i in unique_ip:
             f.write(i + "\n")
+            
+        f.write("HTTP CODE FREQUENCY:\n")
+        for code,count in code_freq.items():
+            f.write(f"{code}:{count}\n")
             
             
             
@@ -47,5 +61,5 @@ def match_ip_errcode():
             if err_code:
                 match.write(f"HTTP ERROR CODE : {err_code.group()}\n")
                 
-match_ip_errcode()
+text_analysis()
                 
